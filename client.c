26e65a41d1ddd86084d8ca0cdc9ddd6129bd27ca
shaojason999 <shaojason999@gmail.com>
2018-11-18 21:37:47 +0800

    Complete with msg send and receive

--- client.c
+++ client.c
@@ -1,8 +1,51 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #include "client.h"
 
 
 int main(int argc, char *argv[])
 {
-	return 0;
-}
+    int sock_fd;
+    char buf[1024];
+    int buf_len=sizeof(buf);
+    /*important! never forget*/
+    memset(buf, 0, buf_len);
 
+    /*set the struct sockaddr_in, including the port and addr of the server*/
+    struct  sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family=AF_INET;
+    serv_addr.sin_port=htons(atoi(argv[6]));	/*solved the problem of big and little endian*/
+    /*stored the address into serv_addr*/
+    if(inet_pton(AF_INET, argv[4], &serv_addr.sin_addr)<=0) {
+        printf("invalid address\n");
+        return -1;
+    }
+
+    /*creat a socket(doesn't know any information about server)*/
+    sock_fd=socket(AF_INET, SOCK_STREAM, 0);
+    if(sock_fd<0) {
+        printf("socket creation is failed\n");
+        return -1;
+    }
+    /*make the socket conneted to the server(let the socket know the serv_addr and serv_port)*/
+    if(connect(sock_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) {
+        printf("connect failed\n");
+        return -1;
+    }
+    char *msg="sent from client";
+    send(sock_fd, msg, strlen(msg), 0);
+    printf("message sent\n");
+    while(recv(sock_fd, buf, buf_len, 0)>0) {
+        printf("%s\n",buf);
+        memset(buf, 0, buf_len);
+    }
+
+    close(sock_fd);
+    return 0;
+}
--- server.c
+++ server.c
@@ -1,11 +1,63 @@
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <unistd.h>
 #include "server.h"
-
 
 
 int main(int argc, char *argv[])
 {
+    /*sock_fd is used to listen to the requests, and the sock_client is connted to the clients actually*/
+    int sock_fd, sock_client;
+    char buf[1024];
+    char *msg="sent from server";
+    int buf_len=sizeof(buf);
+    /*important! never forget*/
+    memset(buf, 0, buf_len);
+
+    struct sockaddr_in my_addr;
+    memset(&my_addr, 0, sizeof(my_addr));
+    my_addr.sin_family=AF_INET;
+    my_addr.sin_port=htons(atoi(argv[4]));	/*solved the problem of big and little endian*/
+    my_addr.sin_addr.s_addr=INADDR_ANY;
+    int my_addr_len=sizeof(my_addr);
+
+    /*creat the socket and set the let the socket know the information about server itself*/
+    sock_fd=socket(AF_INET, SOCK_STREAM, 0);
+    if(sock_fd<0) {
+        printf("socket creation is failed\n");
+        exit(-1);
+    }
+    if(bind(sock_fd, (struct sockaddr*)&my_addr, sizeof(my_addr))<0) {
+        printf("bind failed\n");
+        exit(-1);
+    }
+
+    /*waiting for the clients*/
+    if(listen(sock_fd, 3)<0) {
+        printf("listen failed\n");
+        exit(-1);
+    }
+
+    /*creat a new socket to recv/send with client*/
+    sock_client=accept(sock_fd, (struct sockaddr*)&my_addr, (socklen_t*)&my_addr_len);
+    if(sock_client<0) {
+        printf("accept failed\n");
+        exit(-1);
+    }
+
+    recv(sock_client, buf, sizeof(buf), 0);
+    printf("%s\n",buf);
+    send(sock_client, msg, strlen(msg), 0);
+    printf("message sent\n");
 
 
+    close(sock_client);
+    close(sock_fd);
+
+    return 0;
 }
 
 
